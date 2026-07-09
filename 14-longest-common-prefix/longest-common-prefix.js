faf32a682165384aeaccab23f1e91f17be893875
Sayed R. Khalid (Al-amin) <sayedrkhalid.official@gmail.com>
2026-07-09 19:55:55 +0600

    Time: 2 ms (44.97%) | Memory: 55.3 MB (28.44%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if(strs.length === 0) return '';
+
+    let prefix = '';
+    const first = strs[0];
+
+    for(let i = 0; i < first.length; i++) {
+
+        for(let j = 1; j < strs.length; j++){
+            if(i >= strs[j].length || first[i] !== strs[j][i]){
+                return prefix;
+            }
+
+        }
+
+        prefix += first[i]
+    }
+
+    return prefix;
+};
